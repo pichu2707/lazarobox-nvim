a65d335d1ad9dbeeea39d2d66dab6306ddcb01ab
pichu2707 <pichu_2707@hotmail.com>
2026-05-06 22:34:07 +0200

    fix: resuelto problema de copia/pega desde nvim a elementos exteriores

--- init.lua
+++ init.lua
@@ -3,3 +3,19 @@ require("config/keymaps")
 require("config.lazy")
 require("config.media-autocmds")
 vim.opt.clipboard = "unnamedplus"
+
+-- Forzar uso de xclip en X11
+if vim.fn.has('unix') == 1 and vim.fn.executable('xclip') == 1 then
+  vim.g.clipboard = {
+    name = 'xclip-system',
+    copy = {
+      ['+'] = 'xclip -sel clip',
+      ['*'] = 'xclip -sel prim',
+    },
+    paste = {
+      ['+'] = 'xclip -sel clip -o',
+      ['*'] = 'xclip -sel prim -o',
+    },
+    cache_enabled = 0,
+  }
+end
